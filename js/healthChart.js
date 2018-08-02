7dc9035c3607be7aa1a3376030740da41a9fd0a0
Jack-Dempster <jackdempster17@gmail.com>
2018-08-02 17:05:13 +0100

    Updated Health page to have modal windows and full amount of charts

--- js/healthChart.js
+++ js/healthChart.js
@@ -3,7 +3,26 @@ google.charts.load('current', { 'packages': ['corechart'] });
 google.charts.setOnLoadCallback(drawSmokeChart);
 google.charts.setOnLoadCallback(drawSmokeDeathChart);
 google.charts.setOnLoadCallback(drawLifeChart);
+google.charts.setOnLoadCallback(drawBeingChart);
 google.charts.setOnLoadCallback(drawBikeChart);
+google.charts.setOnLoadCallback(drawCoronChart);
+google.charts.setOnLoadCallback(drawCancerChart);
+google.charts.setOnLoadCallback(drawCerebChart);
+
+// Get the modal
+var modal = document.getElementById('');
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.visibility = "hidden";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style. visibility = "hidden";
+    }
+}
 
 //Draw Smoking Quitter chart
 function drawSmokeChart() {
@@ -20,23 +39,22 @@ function drawSmokeChart() {
         }
 
         var options = {
-            title: 'Smoking Quitters London',
+            title: 'Smoking Quit Rates',
             vAxis: { title: 'Quitters per 100,000' },
             hAxis: { title: '', slantedText: true, slantedTextAngle: 45 },
             series: 
             {
                 0: {lineDashStyle: [4, 4]},
-            },
-            explorer: {
-                    actions: ['dragToZoom', 'rightClickToReset'],
-                    maxZoomIn: 0.5,
-                    keepInBound: true
             }
         };
 
         var data = response.getDataTable();
         var chart = new google.visualization.LineChart(document.getElementById('smokeChart'));
         chart.draw(data, google.charts.Line.convertOptions(options));
+        google.visualization.events.addListener(chart, 'click', function(){
+            modal=document.getElementById('smokeQuitModal');
+            modal.style.visibility = "visible";
+        })
     }
 }
 function drawSmokeDeathChart() {
@@ -57,11 +75,6 @@ function drawSmokeDeathChart() {
             series: 
             {
                 0: {lineDashStyle: [4, 4]},
-            },
-            explorer: {
-                    actions: ['dragToZoom', 'rightClickToReset'],
-                    maxZoomIn: 0.5,
-                    keepInBound: true
             }
             //chartArea:{width:'80%', height:'80%' } 
         };
@@ -69,6 +82,10 @@ function drawSmokeDeathChart() {
         var data = response.getDataTable();
         var chart = new google.visualization.LineChart(document.getElementById('smokeDeath'));
         chart.draw(data, google.charts.Line.convertOptions(options));
+        google.visualization.events.addListener(chart, 'click', function(){
+            modal=document.getElementById('smokeDeathModal');
+            modal.style.visibility = "visible";
+        })
     }
 }
 
@@ -91,17 +108,16 @@ function drawLifeChart() {
             {
                 0: {lineDashStyle: [4, 4]},
                 3: {lineDashStyle: [4,4]}
-            },
-            explorer: {
-                actions: ['dragToZoom', 'rightClickToReset'],
-                maxZoomIn: 0.5,
-                keepInBound: true
-        }
+            }
         };
 
         var data = response.getDataTable();
         var chart = new google.visualization.LineChart(document.getElementById('lifeExpctChart'));
         chart.draw(data, google.charts.Line.convertOptions(options));
+        google.visualization.events.addListener(chart, 'click', function(){
+            modal=document.getElementById('lifeExpModal');
+            modal.style.visibility = "visible";
+        })
     }
 }
 
@@ -119,17 +135,124 @@ function drawBikeChart() {
         var options = {
             title: 'Bicycle Hire',
             vAxis: { title: '' },
-            hAxis: { title: '', slantedText: true, slantedTextAngle: 45 },
-            explorer: {
-                actions: ['dragToZoom', 'rightClickToReset'],
-                maxZoomIn: 0.5,
-                keepInBound: true
-        }
+            hAxis: { title: '', slantedText: true, slantedTextAngle: 45 }
         };
 
         var data = response.getDataTable();
         var chart = new google.visualization.LineChart(document.getElementById('bikeChart'));
         chart.draw(data, google.charts.Line.convertOptions(options));
+        google.visualization.events.addListener(chart, 'click', function(){
+            modal=document.getElementById('bikeModal');
+            modal.style.visibility = "visible";
+        })
+    }
+}
+
+function drawCoronChart() {
+    var query = new google.visualization.Query(
+        'https://docs.google.com/spreadsheets/d/10ax6KeegFRMbyHCy2PjFzg9GlmF28Gq-ul45FP9er8E/edit?usp=sharing');
+    query.send(handleQueryResponse);
+
+    function handleQueryResponse(response) {
+        if (response.isError()) {
+            alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+            return;
+        }
+
+        var options = {
+            title: 'Early Deaths from Coronary Heart Disease',
+            vAxis: { title: 'Standarised Rate' },
+            hAxis: { title: '', slantedText: true, slantedTextAngle: 45 }
+        };
+
+        var data = response.getDataTable();
+        var chart = new google.visualization.LineChart(document.getElementById('coronChart'));
+        chart.draw(data, google.charts.Line.convertOptions(options));
+        google.visualization.events.addListener(chart, 'click', function(){
+            modal=document.getElementById('coronModal');
+            modal.style.visibility = "visible";
+        })
+    }
+}
+
+function drawCancerChart() {
+    var query = new google.visualization.Query(
+        'https://docs.google.com/spreadsheets/d/1j2wypCbEIBJ2SX63u5OXCorUMSWS51-qZAqUCQ59Ufc/edit?usp=sharing');
+    query.send(handleQueryResponse);
+
+    function handleQueryResponse(response) {
+        if (response.isError()) {
+            alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+            return;
+        }
+
+        var options = {
+            title: 'Early Deaths from Cancer',
+            vAxis: { title: 'Standarised Rate' },
+            hAxis: { title: '', slantedText: true, slantedTextAngle: 45 }
+        };
+
+        var data = response.getDataTable();
+        var chart = new google.visualization.LineChart(document.getElementById('cancerChart'));
+        chart.draw(data, google.charts.Line.convertOptions(options));
+        google.visualization.events.addListener(chart, 'click', function(){
+            modal=document.getElementById('cancerModal');
+            modal.style.visibility = "visible";
+        })
+    }
+}
+
+function drawCerebChart() {
+    var query = new google.visualization.Query(
+        'https://docs.google.com/spreadsheets/d/1Z0E3-K3GUCaqvOoUYXjUNKfhJ950YtlMNOmxRKtLUeY/edit?usp=sharing');
+    query.send(handleQueryResponse);
+
+    function handleQueryResponse(response) {
+        if (response.isError()) {
+            alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+            return;
+        }
+
+        var options = {
+            title: 'Early Deaths from Cerebrovascular Disease',
+            vAxis: { title: 'Standarised Rate' },
+            hAxis: { title: '', slantedText: true, slantedTextAngle: 45 }
+        };
+
+        var data = response.getDataTable();
+        var chart = new google.visualization.LineChart(document.getElementById('cerebChart'));
+        chart.draw(data, google.charts.Line.convertOptions(options));
+        google.visualization.events.addListener(chart, 'click', function(){
+            modal=document.getElementById('cerebModal');
+            modal.style.visibility = "visible";
+        })
+    }
+}
+
+function drawBeingChart() {
+    var query = new google.visualization.Query(
+        'https://docs.google.com/spreadsheets/d/1NAbz0hhc0GhATSg3vVB3ru2yFw2TNDICH185HS43jaw/edit?usp=sharing');
+    query.send(handleQueryResponse);
+
+    function handleQueryResponse(response) {
+        if (response.isError()) {
+            alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+            return;
+        }
+
+        var options = {
+            title: 'General Health of Glasgow Residents',
+            vAxis: { title: '' },
+            hAxis: { title: '', slantedText: true, slantedTextAngle: 45 }
+        };
+
+        var data = response.getDataTable();
+        var chart = new google.visualization.PieChart(document.getElementById('beingChart'));
+        chart.draw(data, google.charts.Line.convertOptions(options));
+        google.visualization.events.addListener(chart, 'click', function(){
+            modal=document.getElementById('beingModal');
+            modal.style.visibility = "visible";
+        })
     }
 }
 
--- js/trafficEventCharts.js
+++ js/trafficEventCharts.js
@@ -1,15 +1,13 @@
 google.charts.load('current', { 'packages': ['line'] });
 google.charts.load('current', { 'packages': ['corechart'] });
 google.charts.setOnLoadCallback(drawSev1Chart);
-google.charts.setOnLoadCallback(drawSev2Chart);
-google.charts.setOnLoadCallback(drawSev3Chart);
 
 function drawSev1Chart() {
 
     var query = new google.visualization.Query(
-        'https://docs.google.com/spreadsheets/d/1QYUUwh_3Yawy-x5V3y_vMOXq1wn9BIOQBDnrBqOLgpI/edit?usp=sharing');
+        'https://docs.google.com/spreadsheets/d/14bZJPaHbZvw1O2mni8Uql3fa1Hm_-ys78m_0dH3EkNw/edit?usp=sharing');
 
-    query.setQuery("select AE, count(F) where F=1 group by AE label count(F) 'Severity 1 Accidents'");
+    //query.setQuery("select AE, count(F) where F=1 group by AE label count(F) 'Severity 1 Accidents'");
     query.send(handleQueryResponse);
 
     function handleQueryResponse(response) {
@@ -22,6 +20,12 @@ function drawSev1Chart() {
             title: 'Traffic Accidents',
             vAxis: { title: '' },
             hAxis: { title: '', slantedText: true, slantedTextAngle: 45 },
+            legend: { position: "bottom" },
+            animation: {
+                "startup": true,
+                duration: 1000,
+                easing: "out"
+            },
             explorer: {
                 actions: ['dragToZoom', 'rightClickToReset'],
                 maxZoomIn: 0.5,
@@ -31,68 +35,6 @@ function drawSev1Chart() {
 
         var data = response.getDataTable();
         var chart = new google.visualization.ColumnChart(document.getElementById('sev1Chart'));
-        chart.draw(data, google.charts.Line.convertOptions(options));
-    }
-}
-
-function drawSev2Chart() {
-
-    var query = new google.visualization.Query(
-        'https://docs.google.com/spreadsheets/d/1QYUUwh_3Yawy-x5V3y_vMOXq1wn9BIOQBDnrBqOLgpI/edit?usp=sharing');
-
-    query.setQuery("select AE, count(F) where F=2 group by AE label count(F) 'Severity 2 Accidents'");
-    query.send(handleQueryResponse);
-
-    function handleQueryResponse(response) {
-        if (response.isError()) {
-            alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
-            return;
-        }
-
-        var options = {
-            title: 'Traffic Accidents',
-            vAxis: { title: '' },
-            hAxis: { title: '', slantedText: true, slantedTextAngle: 45 },
-            explorer: {
-                actions: ['dragToZoom', 'rightClickToReset'],
-                maxZoomIn: 0.5,
-                keepInBound: true
-            }
-        };
-
-        var data = response.getDataTable();
-        var chart = new google.visualization.ColumnChart(document.getElementById('sev2Chart'));
-        chart.draw(data, google.charts.Line.convertOptions(options));
-    }
-}
-
-function drawSev3Chart() {
-
-    var query = new google.visualization.Query(
-        'https://docs.google.com/spreadsheets/d/1QYUUwh_3Yawy-x5V3y_vMOXq1wn9BIOQBDnrBqOLgpI/edit?usp=sharing');
-
-    query.setQuery("select AE, count(F) where F=3 group by AE label count(F) 'Severity 3 Accidents'");
-    query.send(handleQueryResponse);
-
-    function handleQueryResponse(response) {
-        if (response.isError()) {
-            alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
-            return;
-        }
-
-        var options = {
-            title: 'Traffic Accidents',
-            vAxis: { title: '' },
-            hAxis: { title: '', slantedText: true, slantedTextAngle: 45 },
-            explorer: {
-                actions: ['dragToZoom', 'rightClickToReset'],
-                maxZoomIn: 0.5,
-                keepInBound: true
-            }
-        };
-
-        var data = response.getDataTable();
-        var chart = new google.visualization.ColumnChart(document.getElementById('sev3Chart'));
         chart.draw(data, google.charts.Line.convertOptions(options));
     }
 }
