b898c9cb8f50b187da5089f9c745bfe485b1e4bd
Jack-Dempster <jackdempster17@gmail.com>
2018-08-03 03:05:49 +0100

    Fixed map and added glasgow bounds

--- js/map.js
+++ js/map.js
@@ -6,52 +6,42 @@ var streets = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?
     {
         attribution: 'Tiles by <a href="http://mapc.org">MAPC</a>, Data by <a href="http://mass.gov/mgis">MassGIS</a>',
         maxZoom: 17,
-        minZoom: 12,
+        minZoom: 8,
         id: 'mapbox.streets',
         accessToken: 'pk.eyJ1IjoiZGVtcHNzIiwiYSI6ImNqazVuMWRsbDAxcjAza3RlZnZtaXczMWwifQ.9EurZ8FEJU_hK046wbSfAQ'
     }).addTo(map);
+map.setZoom(11);
 
-var satellite = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}',
-    {
-        attribution: 'Tiles by <a href="http://mapc.org">MAPC</a>, Data by <a href="http://mass.gov/mgis">MassGIS</a>',
-        maxZoom: 17,
-        minZoom: 12,
-        id: 'mapbox.satellite',
-        accessToken: 'pk.eyJ1IjoiZGVtcHNzIiwiYSI6ImNqazVuMWRsbDAxcjAza3RlZnZtaXczMWwifQ.9EurZ8FEJU_hK046wbSfAQ'
-    });
-map.setZoom(14);
+var northEast = new L.GeoJSON.AJAX("https://mapit.mysociety.org/area/151265.geojson"),
+    greaterPollock = new L.GeoJSON.AJAX("https://mapit.mysociety.org/area/151266.geojson"),
+    newlands = new L.GeoJSON.AJAX("https://mapit.mysociety.org/area/151267.geojson"),
+    pollokshields = new L.GeoJSON.AJAX("https://mapit.mysociety.org/area/151268.geojson"),
+    linn = new L.GeoJSON.AJAX("https://mapit.mysociety.org/area/151269.geojson"),
+    langside = new L.GeoJSON.AJAX("https://mapit.mysociety.org/area/151270.geojson"),
+    southside = new L.GeoJSON.AJAX("https://mapit.mysociety.org/area/151271.geojson"),
+    shettleston = new L.GeoJSON.AJAX("https://mapit.mysociety.org/area/151272.geojson"),
+    cardonald = new L.GeoJSON.AJAX("https://mapit.mysociety.org/area/151273.geojson"),
+    govan = new L.GeoJSON.AJAX("https://mapit.mysociety.org/area/151274.geojson"),
+    garscadden = new L.GeoJSON.AJAX("https://mapit.mysociety.org/area/151275.geojson"),
+    victoria = new L.GeoJSON.AJAX("https://mapit.mysociety.org/area/151276.geojson"),
+    maryhill = new L.GeoJSON.AJAX("https://mapit.mysociety.org/area/151277.geojson"),
+    drumpchapel = new L.GeoJSON.AJAX("https://mapit.mysociety.org/area/151278.geojson"),
+    calton = new L.GeoJSON.AJAX("https://mapit.mysociety.org/area/151279.geojson"),
+    baillieston = new L.GeoJSON.AJAX("https://mapit.mysociety.org/area/151280.geojson"),
+    east = new L.GeoJSON.AJAX("https://mapit.mysociety.org/area/151281.geojson"),
+    canal = new L.GeoJSON.AJAX("https://mapit.mysociety.org/area/151282.geojson"),
+    springburn = new L.GeoJSON.AJAX("https://mapit.mysociety.org/area/151283.geojson"),
+    anderston = new L.GeoJSON.AJAX("https://mapit.mysociety.org/area/151284.geojson"),
+    partick = new L.GeoJSON.AJAX("https://mapit.mysociety.org/area/151285.geojson"),
+    hillhead = new L.GeoJSON.AJAX("https://mapit.mysociety.org/area/151286.geojson");
 
-$.getJSON("/data/traffic.geojson", function (data) {
+var wards = L.layerGroup([northEast, greaterPollock, newlands, pollokshields, linn, langside, southside, shettleston, cardonald, govan, garscadden, victoria, maryhill, drumpchapel, calton, baillieston, east, canal, springburn, anderston, partick, hillhead]).addTo(map);
 
-    var locations = data.features.map(function (rat) {
-        // the heatmap plugin wants an array of each location
-        var location = rat.geometry.coordinates.reverse();
-        location.push(0.5);
-        return location; // e.g. [50.5, 30.5, 0.2], // lat, lng, intensity
-    });
-    var cctv = new L.GeoJSON.AJAX("/data/cctv-locations.geojson", {
-        pointToLayer: function (feature, latlng) {
-            return L.circleMarker(latlng, {
-                radius: 10
-            });
-        }
-    });
+var baseMaps = {
+    "Streets" : streets
+};
+var overLayMaps = {
+    "Wards" : wards
+};
 
-    var heat = L.heatLayer(locations, { radius: 10 });
-    map.addLayer(heat);
-    var baseMaps = {
-        "Streets": streets,
-        "Satellite": satellite
-    };
-
-    var overlayMaps = {
-        "Heat": heat,
-        "CCTV": cctv
-    };
-
-    L.control.layers(baseMaps, overlayMaps).addTo(map);
-})
-
-
-
-
+L.control.layers(baseMaps, overLayMaps).addTo(map);
